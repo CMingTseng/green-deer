fc65382c768f13502e13650f26cb5cd987ae7e4f
Krzysztof Antoniak <krzysztof@antoniak.in>
2017-09-16 06:43:52 +0200

    Add Google Proto Buffers

--- app/src/main/java/com/kantoniak/greendeer/HomeActivityFragment.java
+++ app/src/main/java/com/kantoniak/greendeer/HomeActivityFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +18,13 @@ public class HomeActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        com.kantoniak.greendeer.proto.Run sampleRun = com.kantoniak.greendeer.proto.Run.newBuilder()
+                .setMeters(10).setWeight(100).build();
+        TextView textView = (TextView)view.findViewById(R.id.hello_world);
+        textView.setText("Hello, " + sampleRun + "!");
+
+        return view;
     }
 }
